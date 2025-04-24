d64d4ae6f1f4e68186a2d55326c9c82ac8f3a7a3
Henri2777 <77494676+Henri2777@users.noreply.github.com>
2025-04-24 18:06:23 +0000

    Add Language DE

---
+++ browser/src/i18n/locales/de.ts
@@ -0,0 +1,66 @@
+const de = {
+    translation: {
+      serial: {
+        notSupported:
+          'Seriell nicht unterstützt. Bitte benutze den Desktop Chrome Browser um Maus und Tastatur zu verwenden.',
+        failed: 'Verbindung zu Seriell fehlgeschlagen. Bitte erneut versuchen'
+      },
+      camera: {
+        tip: 'Warten auf Berechtigung...',
+        denied: 'Berechtigung verweigert',
+        authorize:
+          'Der Remote-Desktop erfordert eine Kamerazulassung. Bitte autorisieren Sie die Kamera in den Browsereinstellungen.',
+        failed: 'Kamera konnte nicht verbunden werden. Bitte versuche es erneut.'
+      },
+      modal: {
+        title: 'USB-Gerät auswählen',
+        selectVideo: 'Bitte wähle ein Video-Eingabegerät aus',
+        selectSerial: 'Serielles Gerät auswählen'
+      },
+      menu: {
+        serial: 'Seriell',
+        keyboard: 'Tastatur',
+        mouse: 'Maus'
+      },
+      video: {
+        resolution: 'Auflösung',
+        customResolution: 'Benutzerdefiniert',
+        device: 'Gerät',
+        custom: {
+          title: 'Benutzerdefinierte Aufösung',
+          width: 'Breite',
+          height: 'Höhe',
+          confirm: 'Ok',
+          cancel: 'Abbrechen'
+        }
+      },
+      keyboard: {
+        paste: 'Einfügen',
+        virtualKeyboard: 'Virtuelle Tastatur',
+        ctrlAltDel: 'Strg + Alt + Entfernen'
+      },
+      mouse: {
+        cursor: {
+          title: 'Mauszeiger',
+          pointer: 'Zeiger',
+          grab: 'Hand',
+          cell: 'Kreuz',
+          hide: 'Versteckt'
+        },
+        mode: 'Mausmodus',
+        absolute: 'Absoluter Modus',
+        relative: 'Relativer Mode',
+        direction: 'Scrollrichtung',
+        scrollUp: 'Hochscrollen',
+        scrollDown: 'Runterscrollen',
+        requestPointer: 'Benutze relativen Modus. Bitte auf den Desktop klicken, um den Mauszeiger anzuzeigen.'
+      },
+      settings: {
+        language: 'Sprache',
+        document: 'Dokument',
+        download: 'Download'
+      }
+    }
+  };
+  
+  export default de;
