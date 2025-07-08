9a7aabb1b6225a5dc0a98695544e7a5b3b57d6cf
triektor <triektor@gmail.com>
2025-07-09 01:35:13 +0200

    Add Language PL

---
+++ browser/src/i18n/locales/pl.ts
@@ -0,0 +1,69 @@
+const pl = {
+  translation: {
+    serial: {
+      notSupported:
+        'Port szereowy nie jest obsługiwany. Proszę użyć przeglądarki Chrome na komputerze, aby włączyć mysz i klawiaturę.',
+      failed: 'Błąd połączenia portu szeregowego. Spróbuj ponownie'
+    },
+    camera: {
+      tip: 'Oczekiwanie na autoryzację...',
+      denied: 'Odmowa dostępu',
+      authorize:
+        'Zdalny pulpit wymaga uprawnień kamery. Proszę autoryzować kamerę w ustawieniach przeglądarki.',
+      failed: 'Nie udało się połączyć kamery. Spróbuj ponownie.'
+    },
+    modal: {
+      title: 'Wybierz port USB urządzenia',
+      selectVideo: 'Proszę wybrać wejściowe wideo urządzenie',
+      selectSerial: 'Wybierz port szeregowy urządzenia'
+    },
+    menu: {
+      serial: 'Port',
+      keyboard: 'Klawiatura',
+      mouse: 'Myszka'
+    },
+    video: {
+      resolution: 'Rozdzielczość',
+      customResolution: 'Niestandardowy',
+      device: 'Urządzenie',
+      custom: {
+        title: 'Niestandardowa rozdzielczość',
+        width: 'Szerokość',
+        height: 'Wysokość',
+        confirm: 'Ok',
+        cancel: 'Anuluj'
+      }
+    },
+    keyboard: {
+      paste: 'Wklej',
+      virtualKeyboard: 'Klawiatura',
+      ctrlAltDel: 'Ctrl + Alt + Delete'
+    },
+    mouse: {
+      cursor: {
+        title: 'Kursor',
+        pointer: 'Standardowy kursor',
+        grab: 'Kursor chwytak',
+        cell: 'Kursor komórki',
+        hide: 'Ukryj kursor'
+      },
+      mode: 'Tryb myszki',
+      absolute: 'Tryb bezwzględny',
+      relative: 'Tryb względny',
+      direction: 'Kierunek koła',
+      scrollUp: 'Przewiń w górę',
+      scrollDown: 'Przewiń w dół',
+      speed: 'Prędkość koła',
+      fast: 'Szybko',
+      slow: 'Wolno',
+      requestPointer: 'Korzystasz z trybu względnego. Kliknij pulpit, aby uzyskać wskaźnik myszy.'
+    },
+    settings: {
+      language: 'Język',
+      document: 'Dokument',
+      download: 'Sciągnij'
+    }
+  }
+};
+
+export default pl;
