df9ce0bfb972e75e16c7cedb8b7f3518c52d71ad
triektor <triektor@gmail.com>
2025-07-09 01:25:31 +0200

    Add language PL for Desktop

---
+++ desktop/src/renderer/src/i18n/locales/pl.ts
@@ -0,0 +1,83 @@
+const pl = {
+  translation: {
+    camera: {
+      tip: 'Oczekiwanie na autoryzację...',
+      denied: 'Autoryzacja nie powiodła się',
+      authorize:
+        'Pulpit zdalny wymaga uprawnień kamery. Proszę udzielić uprawnień kamery w ustawieniach.',
+      failed: 'Nie udało się połączyć kamery. Spróbuj ponownie.'
+    },
+    modal: {
+      title: 'Wybierz USB urządzenia',
+      selectVideo: 'Proszę wybrać wejściowe wideo urządzenia',
+      selectSerial: 'Proszę wybrać port urządzenia'
+    },
+    menu: {
+      serial: 'Port',
+      keyboard: 'Klawiatura',
+      mouse: 'Myszka'
+    },
+    video: {
+      resolution: 'Rozdzielczość',
+      customResolution: 'Niestandardowy',
+      device: 'Urządzenie',
+      custom: {
+        title: 'Niestandardowa rozdzielczość',
+        width: 'Szerokość',
+        height: 'Wysokość',
+        confirm: 'Ok',
+        cancel: 'Anuluj'
+      }
+    },
+    keyboard: {
+      paste: 'Wklej',
+      virtualKeyboard: 'Klawiatura',
+      ctrlAltDel: 'Ctrl + Alt + Delete'
+    },
+    mouse: {
+      cursor: {
+        title: 'Kursor',
+        pointer: 'Standardowy kursor',
+        grab: 'Kursor chwytak',
+        cell: 'Kursor korórki',
+        hide: 'Ukryj kursor'
+      },
+      mode: 'Tryb myszki',
+      absolute: 'Tryb bezwzględny',
+      relative: 'Tryb względny',
+      direction: 'Kierunek koła',
+      scrollUp: 'Przewiń w górę',
+      scrollDown: 'Przewiń w dół',
+      speed: 'Prędkość koła',
+      fast: 'Szybko',
+      slow: 'Wolno',
+      requestPointer: 'Korzystanie z trybu względnego. Kliknij pulpit, aby uzyskać wskaźnik myszy.'
+    },
+    settings: {
+      title: 'Ustawienia',
+      appearance: {
+        title: 'Wygląd',
+        language: 'Język',
+        menu: 'Pasek menu',
+        menuTips: 'Otwórz pasek menu po uruchomieniu'
+      },
+      update: {
+        title: 'Sprawdź aktualizacje',
+        latest: 'Oprogramowanie jest aktualne.',
+        outdated: 'Aktualizacja jest dostępna. Czy na pewno chcesz dokonać aktualizacji?',
+        downloading: 'Ściąganie...',
+        installing: 'Instalacja...',
+        failed: 'Aktualizacja nie powiodła się. Proszę spróbować ponownie.',
+        confirm: 'Potwierdzam',
+        cancel: 'Anuluj'
+      },
+      about: {
+        title: 'Informacje',
+        version: 'Wersja',
+        community: 'Współpraca'
+      }
+    }
+  }
+}
+
+export default pl
